d7cc301f2e95749c250b3df56ad87128f6d0a311
Will <wmaccarty@gmail.com>
2020-10-06 11:41:50 -0400

    set up material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
+
 
 import UserList from './components/UsersList';
 
 
 function App() {
-  console.log("____Rendering app_____")
-  return (
-    <BrowserRouter>
-        <nav>
-            <ul>
-                <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
-            </ul>
-        </nav>
-        <Switch>
-            <Route path="/users">
-                <UserList />
-            </Route>
+    console.log("____Rendering app_____")
+    return (
+        <>
+            <CssBaseline />
+            <BrowserRouter>
+                <nav>
+                    <ul>
+                        <li><NavLink to="/" activeclass="active">Home</NavLink></li>
+                        <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/users">
+                        <UserList />
+                    </Route>
 
-            <Route path="/">
-                <h1>My Home Page</h1>
-            </Route>
-        </Switch>
-    </BrowserRouter>
-  );
+                    <Route path="/">
+                        <h1>My Home Page</h1>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
